557b3c98841ea1f3a5f41187350fa025202299a9
hacker4257 <wuzhifeng95@gmail.com>
2026-02-04 21:12:12 +0800

    fix show

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   plugins: [react()],
-  base: process.env.VITE_BASE || '/',
-})
+  base: command === 'build' ? process.env.VITE_BASE || './' : '/',
+}))
